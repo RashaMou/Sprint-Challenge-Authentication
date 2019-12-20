87418f770a549dec08a494d21c54b77c00356ab0
Rasha Moumneh <rasha@fastmail.com>
2019-12-20 12:16:48 -0500

    Add authenticate middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -4,5 +4,20 @@
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const { authorization } = req.headers;
+
+  if (authorization) {
+    const secret = process.env.JWT_SECRET || "is it secret, is it safe?";
+
+    jwt.verify(authorization, secret, function(err, decodedToken) {
+      if (err) {
+        res.status(401).json({ you: "shall not pass!" });
+      } else {
+        req.token = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "Please login and try again" });
+  }
 };
