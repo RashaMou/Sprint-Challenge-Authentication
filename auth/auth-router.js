23d3b546e827210ac3154cc004219fff03dff641
Rasha Moumneh <rasha@fastmail.com>
2019-12-20 12:45:05 -0500

    Implement registeration validation middleware

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken"); // installed this
-
+const validateRegistration = require("./register-middleware");
 const Users = require("../users/users-model");
 
 router.get("/users", async (req, res) => {
@@ -17,7 +17,7 @@ router.get("/users", async (req, res) => {
   }
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", validateRegistration, (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
